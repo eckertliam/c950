583256482a58e94f47789ef1fbab83431ca407a7
Liam Eckert <liameckert17@gmail.com>
2024-07-13 15:38:37 -0400

    normalizing data and working out some of the data structures

--- package.py
+++ package.py
@@ -1,14 +1,46 @@
 from dataclasses import dataclass
+from typing import Optional, Union, List
 from datetime import time
-from typing import Optional
 
 
 @dataclass
 class Package:
-    """Represents a package to be delivered"""
     id: int
     address: int
     weight: float
     deadline: Optional[time] = None
-    delivery_time: Optional[time] = None
-    delivered: bool = False
+    note: Optional[Union['RequireTruck', 'Delay', 'DeliveredWith']] = None
+
+
+@dataclass
+class RequireTruck:
+    truck_id: int
+
+
+@dataclass
+class Delay:
+    delay_time: time
+
+
+@dataclass
+class DeliveredWith:
+    package_ids: List[int]
+
+
+def parse_pack_from_row(row) -> Package:
+    pack_id = int(row[0])
+    address = int(row[1])
+    deadline = time.fromisoformat(row[2]) if row[2] else None
+    weight = float(row[3])
+    raw_note = row[4]
+    note = None
+    if raw_note:
+        # split note by space
+        note_parts = raw_note.split(' ')
+        if note_parts[0] == 'TRUCK':
+            note = RequireTruck(int(note_parts[1]))
+        elif note_parts[0] == 'DELAYED':
+            note = Delay(time.fromisoformat(note_parts[1]))
+        elif note_parts[0] == 'PACK':
+            note = DeliveredWith([int(x) for x in note_parts[1:]])
+    return Package(pack_id, address, weight, deadline, note)
