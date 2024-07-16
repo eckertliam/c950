01d18e0d943784e1ff1debbbcdf63bd8d64d3f96
Liam Eckert <liameckert17@gmail.com>
2024-07-16 14:24:27 -0400

    simplifying many data structures and cutting out Map where I can

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from address_id import AddressIdTable
-from package import PackageTable
-from distance import DistanceTable
+from typing import List, Tuple
+import csv
 
-address_id_table = AddressIdTable('data/address_id.csv')
-package_table = PackageTable(address_id_table, 'data/packages.csv')
-distance_table = DistanceTable('data/distances.csv')
+
+DistanceMatrix = List[List[float]]
+
+
+def distance_matrix_from_csv(file_path: str) -> DistanceMatrix:
+    matrix: DistanceMatrix = []
+    reader = csv.reader(open(file_path, 'r'))
+    # skip header
+    next(reader)
+    for row in reader:
+        # trim the first element
+        row = row[1:]
+        new_row = [float(distance) for distance in row]
+        matrix.append(new_row)
+    return matrix
+
+
+AddressIdMap = List[str]
+
+
+def address_id_from_csv(file_path: str) -> AddressIdMap:
+    """Creates an address id map that is O(1) to access by id"""
+    address_id_map: List[Tuple[int, str]] = []
+    id_max = 0
+    reader = csv.reader(open(file_path, 'r'))
+    for row in reader:
+        addr_id = int(row[1])
+        address_id_map.append((addr_id, row[0]))
+        if id > id_max:
+            id_max = id
+    # create a list of size id_max + 1
+    address_id_list = ['' for _ in range(id_max + 1)]
+    for addr_id, address in address_id_map:
+        address_id_list[addr_id] = address
+    return address_id_list
